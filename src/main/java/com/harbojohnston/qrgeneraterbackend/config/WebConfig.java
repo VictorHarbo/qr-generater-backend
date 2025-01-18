ba8ec2ab904cc94ca40c4a6d6abbed44e2c12d0a
VictorHarbo <vholesen@hotmail.com>
2025-01-18 22:53:37 +0100

    update cors mappings

--- src/main/java/com/harbojohnston/qrgeneraterbackend/config/WebConfig.java
+++ src/main/java/com/harbojohnston/qrgeneraterbackend/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/payment/**")
-                        .allowedOrigins("http://localhost:5173", "https//:qr-generater.com", "https:www.qr-generater.com") // Replace with your frontend URL
+                        .allowedOrigins("http://localhost:5173", "https//:qr-generater.com", "https:www.qr-generater.com", "htttp://localhost:8080", "http://qr-generater-frontend", "http://qr-generater-backend") // Replace with your frontend URL
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
